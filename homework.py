b71cb5e7a48c097b108b9e02e7f0e04417adc93a
Vitaly Myalkin <myalych@mail.ru>
2022-04-06 17:50:17 +0300

    hw oop classinfomessage fixed

--- homework.py
+++ homework.py
@@ -1,33 +1,28 @@
-from typing import Dict, Type, List
+from typing import Dict, Type, List, ClassVar
+from dataclasses import dataclass, asdict
 
 
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+    INFO_STRING: ClassVar[float] = ('Тип тренировки: {training_type}; '
+                                    'Длительность: {duration:.3f} ч.; '
+                                    'Дистанция: {distance:.3f} км; '
+                                    'Ср. скорость: {speed:.3f} км/ч; '
+                                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         """Формируем строку сообщения."""
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        return self.INFO_STRING.format(**asdict(self))
 
 
 class Training:
     """Базовый класс тренировки."""
-
     LEN_STEP: float = 0.65
     M_IN_KM: float = 1000
     H_IN_M: float = 60
@@ -43,11 +38,6 @@ class Training:
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
         return self.action * self.LEN_STEP / self.M_IN_KM
-        # тут раньше была переменная "distance".
-        # мы ее убрали, чтобы она не занимала память?
-        # а высчитанное этой функцией значение
-        # разве не сохраняется в какую-то ячейку?
-        # и, соответственно, опять займет память...
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
@@ -56,11 +46,10 @@ class Training:
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         raise NotImplementedError(
-            'Определите get_spent_calories в %s.' % self.__class__.__name__)
+            f'Определите get_spent_calories в {self.__class__.__name__}')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-
         info = InfoMessage(self.__class__.__name__,
                            self.duration, self.get_distance(),
                            self.get_mean_speed(), self.get_spent_calories())
@@ -69,16 +58,8 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-
     COEFF_CALORIE_1: int = 18
     COEFF_CALORIE_2: int = 20
-
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float) -> None:
-        """Класс бег - наследник тренировки."""
-        super().__init__(action, duration, weight)
 
     def get_spent_calories(self) -> float:
         """Расчет калорий бега."""
@@ -89,7 +70,6 @@ class Running(Training):
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-
     COEFF_WALK_1: float = 0.035
     COEFF_WALK_2: float = 0.029
 
@@ -108,13 +88,10 @@ class SportsWalking(Training):
                  + (self.get_mean_speed() ** 2 // self.height)
                  * self.COEFF_WALK_2 * self.weight)
                 * self.duration * self.H_IN_M)
-    # если здесь использование функции math.sqrt неверно,
-    # то почему значение калорий рассчитывается правильно??
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-
     LEN_STEP: float = 1.38
     COEFF_SWIM_1: float = 1.1
     COEFF_SWIM_2: float = 2
@@ -144,9 +121,6 @@ class Swimming(Training):
 def read_package(workout_type: str,
                  data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-
-    types: str = ''
-
     workout_types: Dict[str, Type[Training]] = {
         'SWM': Swimming,
         'RUN': Running,
@@ -154,13 +128,11 @@ def read_package(workout_type: str,
     }
     if workout_type in workout_types:
         return workout_types[workout_type](*data)
-    else:
-        for i in list(workout_types):
-            types += f'{i}, '
-        raise ValueError(
-            f'Текущая версия модуля фитнес-трекера работает c данными '
-            f'тренировок c кодами {types}а от датчиков поступили '
-            f'данные о неизвестной тренировке с кодом {workout_type}.')
+    types: str = ', '.join(workout_types)
+    raise ValueError(
+        f'Текущая версия модуля фитнес-трекера работает c данными '
+        f'тренировок c кодами {types}, а от датчиков поступили '
+        f'данные о неизвестной тренировке с кодом {workout_type}.')
 
 
 def main(training: Training) -> None:
